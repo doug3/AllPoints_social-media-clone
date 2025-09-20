8bef465d8c9344aa63e85db53cc2dddb1c158c5a
doug3 <doug.corcoran@hotmail.com>
2025-09-20 12:57:17 -0400

    version update commit

--- server/server.js
+++ server/server.js
@@ -20,5 +20,5 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
